f58992525197a77fc30568dcf25f86a35eef773e
Artem Ponamarchuk <Panfil0k@yandex.ru>
2022-06-10 15:50:42 +0300

    fix: замечания ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,19 @@ let editProfileBtn = document.querySelector('.profile__edit-btn');
 let editForm = document.querySelector('.edit-form');
 let closeEditFormBtn = editForm.querySelector('.edit-form__reset-btn');
 let saveEditFormBtn = editForm.querySelector('.edit-form__submit-btn');
-let fixBody = document.querySelector('.page');
 let nameInput = editForm.querySelector('.edit-form__item_el_name');
 let jobInput = editForm.querySelector('.edit-form__item_el_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
 function visibleEditForm() {
-  editForm.classList.remove('form-overlay_close');
-  fixBody.classList.add('fixed');
+  editForm.classList.add('form-overlay_open');
 };
 
 editProfileBtn.addEventListener('click', visibleEditForm);
 
 function closeEditForm() {
-  editForm.classList.add('form-overlay_close');
-  fixBody.classList.remove('fixed');
+  editForm.classList.remove('form-overlay_open');
 };
 
 closeEditFormBtn.addEventListener('click', closeEditForm);
@@ -28,8 +25,7 @@ function formSubmitHandler (evt) {
   let newJob = jobInput.value;
   profileName.textContent = newName;
   profileJob.textContent = newJob;
-  editForm.classList.add('form-overlay_close');
-  fixBody.classList.remove('fixed');
+  closeEditForm();
 }
 
 editForm.addEventListener('submit', formSubmitHandler);
