db6c324b1e99e2689c35c8c8f017b3cd6afee932
Artem Ponamarchuk <Panfil0k@yandex.ru>
2022-06-27 13:11:45 +0300

    fix: правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -15,12 +15,26 @@ const jobInput = popupEditProfile.querySelector('.edit-form__item_el_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
+const placesItemContainer = document.querySelector('.places');
+const placesItemTemplate = document.querySelector('#places-item-template').content;
+
+const nameCardInput = popupAddCard.querySelector('.edit-form__item_el_card-name');
+const srcCardInput = popupAddCard.querySelector('.edit-form__item_el_card-src');
+
+const popupImage = popupImg.querySelector('.popup-img__foto');
+const popupTitle = popupImg.querySelector('.popup-img__title');
+
 /* Открытие\закрытие попапов */
 function openPopup (popup) {
   popup.classList.add('popup_open');
 }
 
-editProfileBtn.addEventListener('click', () => openPopup(popupEditProfile));
+editProfileBtn.addEventListener('click', function() {
+  openPopup(popupEditProfile);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
+
 addCardBtn.addEventListener('click', () => openPopup(popupAddCard));
 
 function closePopup (popup) {
@@ -34,87 +48,42 @@ closePopupImgBtn.addEventListener('click', () => closePopup(popupImg))
 /* Функция редактирования профиля */
 function editProfileSubmitHandler (evt) {
   evt.preventDefault();
-  let newName = nameInput.value;
-  let newJob = jobInput.value;
-  profileName.textContent = newName;
-  profileJob.textContent = newJob;
-  nameInput.value = newName;
-  jobInput.value = newJob;
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
   closePopup(popupEditProfile);
 }
 
 popupEditProfile.addEventListener('submit', editProfileSubmitHandler);
 
-/* Массив карточек */
-const initialCards = [
-  {
-    name: 'Байкал',
-    link: 'https://images.unsplash.com/photo-1551845041-63e8e76836ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://images.unsplash.com/photo-1535427284698-c8e68a1eb910?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1468&q=80'
-  },
-  {
-    name: 'Республика Коми',
-    link: 'https://images.unsplash.com/photo-1525302220185-c387a117886e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80'
-  },
-  {
-    name: 'Тюмень',
-    link: 'https://images.unsplash.com/photo-1621878983992-bac95a1e8dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'
-  },
-  {
-    name: 'Таганай',
-    link: 'https://images.unsplash.com/photo-1521531105925-7c51dffd5098?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80'
-  },
-  {
-    name: 'Оренбург',
-    link: 'https://images.unsplash.com/photo-1601237136101-303aae60375e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80'
-  }
-];
-
-/* Определяем Template и подгружам карточки из массива на страницу */
-const placesItemContainer = document.querySelector('.places');
-const placesItemTemplate = document.querySelector('#places-item-template').content;
-
-initialCards.forEach(function (place) {
-  const placesItemElement = placesItemTemplate.querySelector('.places__item').cloneNode(true);
+/* Создание карточки */
+function createCard(src, title) {
+  const placesItemElement = placesItemTemplate.cloneNode(true);
   
-  placesItemElement.querySelector('.places__foto').src = place.link;
-  placesItemElement.querySelector('.places__foto').alt = place.name;
-  placesItemElement.querySelector('.places__name').textContent = place.name;
+  placesItemElement.querySelector('.places__foto').src = src;
+  placesItemElement.querySelector('.places__foto').alt = title;
+  placesItemElement.querySelector('.places__name').textContent = title;
   
   likeEventListener(placesItemElement);
   openPopupImgListener(placesItemElement);
   deleteEventListener(placesItemElement);
+  
+  return placesItemElement
+}
 
-  placesItemContainer.append(placesItemElement);
+/* Берем Template и подгружам карточки из массива на страницу */
+initialCards.forEach(function (place) {
+  
+  placesItemContainer.append(createCard(place.link, place.name));
+
 });
 
 /* Добавление новых карточек */
-const nameCardInput = popupAddCard.querySelector('.edit-form__item_el_card-name');
-const srcCardInput = popupAddCard.querySelector('.edit-form__item_el_card-src');
-
 function addCardSubmitHandler (evt) {
   evt.preventDefault();
-
-  let newCardName = nameCardInput.value;
-  let newSrc = srcCardInput.value;
-
-  const placesItemElement = placesItemTemplate.querySelector('.places__item').cloneNode(true);
   
-  placesItemElement.querySelector('.places__foto').src = newSrc;
-  placesItemElement.querySelector('.places__foto').alt = newCardName;
-  placesItemElement.querySelector('.places__name').textContent = newCardName;
-
-  likeEventListener(placesItemElement);
-  openPopupImgListener(placesItemElement);
-  deleteEventListener(placesItemElement);
-
-  placesItemContainer.prepend(placesItemElement);
+  placesItemContainer.prepend(createCard(srcCardInput.value, nameCardInput.value));
   
-  nameCardInput.value = '';
-  srcCardInput.value = '';
+  popupAddCard.querySelector('.edit-form').reset();
 
   closePopup(popupAddCard);
 }
@@ -142,30 +111,19 @@ function deleteEventListener(placesItemElement) {
 }
 
 /* Открытие попапа с фото */
-function openPopupImg (evt) {
-
-  openPopup(popupImg);
-
-  let newSrc = evt.target.src;
-  let popupImage = popupImg.querySelector('.popup-img__foto');
-  popupImage.src = newSrc;
-
-  let thisItem = evt.target.closest('.places__item');
-  let newTitle = thisItem.querySelector('.places__name').textContent;
-  let popupTitle = popupImg.querySelector('.popup-img__title');
-  popupTitle.textContent = newTitle;
-}
-
 function openPopupImgListener(placesItemElement) {
-  const imgSmall = placesItemElement.querySelector('.places__foto');
-  imgSmall.addEventListener('click', openPopupImg);
-}
+  const thisImg = placesItemElement.querySelector('.places__foto');
+  const thisTitle = placesItemElement.querySelector('.places__name').textContent;
 
+  function openPopupImg() {
 
-
-
-
-
-
-
+    openPopup(popupImg);
   
+    popupImage.src = thisImg.src;
+    popupImage.alt = thisImg.alt;
+    popupTitle.textContent = thisTitle;
+    
+  }
+
+  thisImg.addEventListener('click', () => openPopupImg(placesItemElement));
+}
---
+++ scripts/initial-сards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: 'Байкал',
+      link: 'https://images.unsplash.com/photo-1551845041-63e8e76836ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://images.unsplash.com/photo-1535427284698-c8e68a1eb910?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1468&q=80'
+    },
+    {
+      name: 'Республика Коми',
+      link: 'https://images.unsplash.com/photo-1525302220185-c387a117886e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80'
+    },
+    {
+      name: 'Тюмень',
+      link: 'https://images.unsplash.com/photo-1621878983992-bac95a1e8dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'
+    },
+    {
+      name: 'Таганай',
+      link: 'https://images.unsplash.com/photo-1521531105925-7c51dffd5098?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80'
+    },
+    {
+      name: 'Оренбург',
+      link: 'https://images.unsplash.com/photo-1601237136101-303aae60375e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80'
+    }
+  ];
